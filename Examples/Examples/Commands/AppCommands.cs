cbfc52007e688dd8d09bc047e0f7d295996fc3c0
Chris <bad_smilie@web.de>
2019-05-28 12:51:59 +0200

    command

---
+++ Examples/Examples/Commands/AppCommands.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Examples
+{
+    class AppCommands
+    {
+        private static ICommand exit = new ExitCommand();
+        private static ICommand exitRouted = new ExitCommandRouted();
+        public static ICommand Exit
+        {
+            get
+            {
+                return exit;
+            }
+        }
+
+        public static ICommand ExitRouted
+        {
+            get
+            {
+                return exitRouted;
+            }
+        }
+    }
+}
---
+++ Examples/Examples/Commands/ExitCommandRouted .cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Examples
+{
+    class ExitCommandRouted : ICommand
+    {
+
+        public event EventHandler CanExecuteChanged;
+        public static readonly RoutedEvent ExecuteEvent = EventManager.RegisterRoutedEvent("Execute", RoutingStrategy.Bubble, typeof(RoutedEventArgs), typeof(ExitCommandRouted));
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            RoutedEventArgs routedEventArgs = new RoutedEventArgs(ExecuteEvent, Keyboard.FocusedElement);
+            Keyboard.FocusedElement.RaiseEvent(routedEventArgs);
+        }
+
+        public static void AddExecuteHandler(DependencyObject o, RoutedEventHandler handler)
+        {
+            ((UIElement)o).AddHandler(ExitCommandRouted.ExecuteEvent, handler);
+        }
+
+        public static void RemoveExecuteHandler(DependencyObject o, RoutedEventHandler handler)
+        {
+            ((UIElement)o).RemoveHandler(ExitCommandRouted.ExecuteEvent, handler);
+        }
+    }
+}
